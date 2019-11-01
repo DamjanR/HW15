e1bac9ff5811468227f208ba53840b6ad6d2fd8d
DR <damjan.ramsak@gmail.com>
2019-11-01 13:14:35 +0100

    HW15

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def prva_stran():
+    return render_template("prva-stran.html")
+
+@app.route("/bogle")
+def bogle():
+    return render_template("bogle.html")
+
+@app.route("/frizer")
+def frizer():
+    return render_template("frizer.html")
+
+@app.route("/fakebook")
+def fakebook():
+    return render_template("fakebook.html")
+
+if __name__ == '__main__':
+    app.run()
